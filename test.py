ddfe2e14286d2b1cf544e68bc642670e3c7b46db
Ana Sofia <anasofia.sofia407@hotmail.com>
2022-05-27 19:59:47 +0100

    Finished unit testing

--- test.py
+++ test.py
@@ -1,23 +1,48 @@
 from Game import newPet, playGame
 from Pet import Pet
 
-def geninputs():
-    inputs = ['i','c','a','a','a','b']
+# Test P1
+def geninputs1():
+    inputs = ['i','n']
     for i in inputs:
         yield i
 
-
 def test_p1(monkeypatch):   
-    GEN = geninputs()
+    GEN = geninputs1()
     monkeypatch.setattr('builtins.input', lambda x: next(GEN))
     words = ["apple", "butter", "cat", "dog", "elephant", "future", "ghost", "history", "icing", "jump"]
     currPet = Pet('name', 1, 10, 10, 10, 0, 3, 3)
-    assert currPet.game2(words[8], [0,1,2])
+    assert currPet.game2(words[8], [0,2,3])
+
+# def test_p2(monkeypatch):   
+#    GEN = geninputs()
+#    monkeypatch.setattr('builtins.input', lambda x: next(GEN))
+#    words = ["apple", "butter", "cat", "dog", "elephant", "future", "ghost", "history", "icing", "jump"]
+#    currPet = Pet('name', 1, 10, 10, 10, 0, 3, 3)
+#    assert currPet.game2(words[8], [0,1,3])
+
+# Test P2
+def geninputs2():
+    inputs = ['a','b', 'c', 'g', 'h', 'o']
+    for i in inputs:
+        yield i
 
 def test_p2(monkeypatch):   
-    GEN = geninputs()
+    GEN = geninputs2()
     monkeypatch.setattr('builtins.input', lambda x: next(GEN))
     words = ["apple", "butter", "cat", "dog", "elephant", "future", "ghost", "history", "icing", "jump"]
     currPet = Pet('name', 1, 10, 10, 10, 0, 3, 3)
-    assert currPet.game2(words[8], [0,1,3])
+    assert currPet.game2(words[6], [0,1,2])
 
+# Test P3
+def geninputs3():
+    inputs = ['d','d', 'a', 'j', 'm', 'p', 'r', 's', 't']
+    for i in inputs:
+        yield i
+
+def test_p3(monkeypatch):   
+    GEN = geninputs3()
+    monkeypatch.setattr('builtins.input', lambda x: next(GEN))
+    words = ["apple", "butter", "cat", "dog", "elephant", "future", "ghost", "history", "icing", "jump"]
+    currPet = Pet('name', 1, 10, 10, 10, 0, 3, 3)
+    assert currPet.game2(words[8], [0,2,3])
